b35a5d1b0a6bff120923e35c75c83dccc30d08d4
Max <maxtvangils@gmail.com>
2021-11-19 15:10:29 +0100

    Switches
    Added switch class

---
+++ src/Hardware/Switch.java
@@ -0,0 +1,27 @@
+package Hardware;
+
+import TI.BoeBot;
+import TI.PinMode;
+
+public class Switch {
+
+    private int pin;
+
+    /**
+     * Reads the state of switches, such as buttons and whiskers
+     * @param pin of the breadboard which the switch uses
+     */
+    public Switch(int pin){
+        this.pin = pin;
+
+        BoeBot.setMode(this.pin, PinMode.Input);
+    }
+
+    /**
+     * Returns the current state of the switch
+     * @return boolean containing the current state of the switch
+     */
+    public boolean getState(){
+        return BoeBot.digitalRead(this.pin);
+    }
+}
