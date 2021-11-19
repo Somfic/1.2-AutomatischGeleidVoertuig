0f125fcc867d31eca520a34055cbfbad63255fd7
Somfic <somfic@gmail.com>
2021-11-19 16:07:01 +0100

    Seperated logic and hardware for Buzzer

--- src/Hardware/Buzzer.java
+++ src/Hardware/Buzzer.java
@@ -9,35 +9,18 @@ import java.sql.Time;
 public class Buzzer {
     private int pin;
     private int frequency;
-    private boolean state;
-    private Timer timer;
 
-    public Buzzer(int pin){
-    this.pin = pin;
-    BoeBot.setMode(pin, PinMode.Output);
-    this.timer = new Timer(0);
-
+    public Buzzer(int pin) {
+        BoeBot.setMode(pin, PinMode.Output);
+        this.pin = pin;
     }
 
-    public void setFrequency(int frequency){
+    public void setFrequency(int frequency) {
         this.frequency = frequency;
-        this.timer.setInterval(frequency);
     }
-    public void buzz (){
 
-            if(timer.timeout()) {
-                timer.mark();
-
-                if(this.state) {
-                    BoeBot.freqOut(this.pin,30000,1000);
-                    this.state = false;
-                    System.out.println("Uit");
-                } else {
-                    this.state = true;
-                    System.out.println("Aan");
-                }
-            }
-        }
-
+    public void buzz(int time) {
+        BoeBot.freqOut(this.pin, this.frequency, time);
     }
+}
 
---
+++ src/Logic/BuzzerLogic.java
@@ -0,0 +1,42 @@
+package Logic;
+
+import Hardware.Buzzer;
+import TI.BoeBot;
+import TI.PinMode;
+import TI.Timer;
+
+public class BuzzerLogic {
+    private int pin;
+
+    private boolean state;
+    private Timer timer;
+
+    private Buzzer buzzer;
+
+    public BuzzerLogic(Buzzer buzzer) {
+        this.buzzer = buzzer;
+    }
+
+    public void setInterval(int interval) {
+        this.timer.setInterval(interval);
+    }
+
+    public void setFrequency(int frequency) {
+        this.buzzer.setFrequency(frequency);
+    }
+
+    public void buzz(int beepLength) {
+        if(timer.timeout()) {
+            timer.mark();
+
+            if(this.state) {
+                this.buzzer.buzz(beepLength);
+                this.state = false;
+                System.out.println("Uit");
+            } else {
+                this.state = true;
+                System.out.println("Aan");
+            }
+        }
+    }
+}
