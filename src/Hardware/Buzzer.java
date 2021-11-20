254eb1917cd7419aa9799bc919af025773660515
Somfic <somfic@gmail.com>
2021-11-20 18:09:47 +0100

    Added comments to the Buzzer class

--- src/Hardware/Buzzer.java
+++ src/Hardware/Buzzer.java
@@ -1,25 +1,36 @@
 package Hardware;
 
-import TI.BoeBot;
-import TI.PinMode;
-import TI.Timer;
-import java.awt.*;
-import java.sql.Time;
+import TI.*;
 
+/**
+ * The Buzzer class is used to control a buzzer.
+ */
 public class Buzzer {
     private int pin;
     private int frequency;
 
+    /**
+     * Constructor for the Buzzer class.
+     * @param pin The pin the buzzer is connected to.
+     */
     public Buzzer(int pin) {
         BoeBot.setMode(pin, PinMode.Output);
         this.pin = pin;
     }
 
+    /**
+     * Sets the frequency of the tone.
+     * @param frequency The frequency of the tone.
+     */
     public void setFrequency(int frequency) {
         this.frequency = frequency;
     }
 
-    public void buzz(int time) {
+    /**
+     * Plays a tone.
+     * @param time The duration of the tone.
+     */
+    public void play(int time) {
         BoeBot.freqOut(this.pin, this.frequency, time);
     }
 }
