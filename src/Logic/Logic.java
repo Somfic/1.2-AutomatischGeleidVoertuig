683447536c6404954d226afb1b4bc7dc95c8a735
Somfic <somfic@gmail.com>
2021-11-20 18:11:57 +0100

    Created Logic interface

---
+++ src/Logic/Logic.java
@@ -0,0 +1,12 @@
+package Logic;
+
+/**
+ * Interface for the logic of the robot.
+ */
+public interface Logic {
+
+    /*
+    * This method is called every tick of the robot's lifecycle.
+    */
+    void process();
+}
