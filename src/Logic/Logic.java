86fd56c94f8425a7950cf1e1a41c8aec2a687609
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-01 18:17:08 +0100

    Comments

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -9,6 +9,7 @@ public interface Logic {
     * This method is called every tick of the robot's lifecycle.
     */
     void process();
+    
     /*
     * This method is called when the hardware needs to be reset.
     */
