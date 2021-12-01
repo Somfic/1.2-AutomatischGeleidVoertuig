a812a5fbb00158dedae3966b78661e62ff2e6642
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-01 18:16:12 +0100

    Removed logic from hardware layer

--- src/Hardware/ServoMotor.java
+++ src/Hardware/ServoMotor.java
@@ -4,42 +4,11 @@ import TI.Servo;
 
 public class ServoMotor {
 
-    private int pin;
-    private int currentSpeed;
-    private int targetSpeed;
-    private boolean reverse;
     private Servo servo;
 
-    private int defaultSpeed = 1500;
-    private int maxSpeed = 1700;
-    private int minSpeed = 1300;
-
-    private int timeInterval = 10;
-
-    public ServoMotor(int pin, boolean reverse) {
-        this.pin = pin;
-        this.currentSpeed = defaultSpeed;
-        this.targetSpeed = defaultSpeed;
-        this.reverse = reverse;
+    public ServoMotor(int pin) {
         this.servo = new Servo(pin);
     }
-
-    public void setTargetSpeed(int targetSpeed){
-        if(targetSpeed > maxSpeed){
-            this.targetSpeed = maxSpeed;
-        }
-        else if(this.targetSpeed < minSpeed){
-            this.targetSpeed = minSpeed;
-        }
-        else {
-            this.targetSpeed = targetSpeed;
-        }
-    }
-
-    public boolean targetSpeedReached(){
-        return this.targetSpeed == this.currentSpeed;
-    }
-
 
     // starts the motor
     public void start() {
@@ -51,27 +20,7 @@ public class ServoMotor {
         this.servo.stop();
     }
 
-    public void setTimeInterval(int time) {
-        this.timeInterval = time;
+    public void set(int pulse) {
+        this.servo.update(pulse);
     }
-
-    //accelerates or decelerates by 10 depending on the boolean given.
-    public void accelerate() {
-        int speedIncrease = this.timeInterval;
-
-        if(this.targetSpeed < this.currentSpeed){
-            speedIncrease = -this.timeInterval;
-        }
-
-
-        // check if the motor is in reverse and changes the speed.
-        if (this.currentSpeed + speedIncrease <= maxSpeed
-                && this.currentSpeed + speedIncrease >= minSpeed
-                && this.targetSpeed != this.currentSpeed) {
-
-            this.servo.update(this.currentSpeed + speedIncrease);
-            this.currentSpeed += speedIncrease;
-
-        }
-    }
- }
+}
