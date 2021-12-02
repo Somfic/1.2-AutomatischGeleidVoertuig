80141ddfd0fae897ab8430d04334376cfc730d42
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-02 14:28:50 +0100

    Added remote buttons for emergency

--- src/Behaviour/EmergencyBehaviour.java
+++ src/Behaviour/EmergencyBehaviour.java
@@ -1,6 +1,8 @@
 package Behaviour;
 
+import Configuration.Config;
 import Hardware.Switch;
+import Logic.InfraredLogic;
 
 public class EmergencyBehaviour implements Behaviour {
 
@@ -8,15 +10,32 @@ public class EmergencyBehaviour implements Behaviour {
     private Switch stopButton;
 
     private boolean isRunning;
+    private InfraredLogic infrared;
+
+
+    public EmergencyBehaviour(InfraredLogic infrared) {
+
+        this.infrared = infrared;
+    }
 
     @Override
     public void initialise() {
-        this.startButton = new Switch(0);
-        this.stopButton = new Switch(1);
+        this.startButton = new Switch(Config.BUTTON_EMERGENCY_STOP_PIN);
+        this.stopButton = new Switch(Config.BUTTON_START_PIN);
     }
 
     @Override
     public void process() {
+        infrared.process();
+
+        if(infrared.getLastCode() == Config.REMOTE_POWER) {
+            this.isRunning = false;
+        }
+
+        if(infrared.getLastCode() == Config.REMOTE_MUTE) {
+            this.isRunning = true;
+        }
+
         if(this.startButton.getState()) {
             this.isRunning = true;
         }
