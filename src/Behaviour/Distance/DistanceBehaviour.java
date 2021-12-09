47252002fc16a2087b05fdc7aa9c2d74ed082a55
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-09 10:31:25 +0100

    Added Ultrasonic

---
+++ src/Behaviour/Distance/DistanceBehaviour.java
@@ -0,0 +1,40 @@
+package Behaviour.Distance;
+
+import Behaviour.Behaviour;
+import Logger.Logger;
+import Logic.DistanceLogic;
+
+public class DistanceBehaviour implements Behaviour {
+
+    private final Logger logger = new Logger(this);
+
+    private DistanceLogic distance;
+
+    private float lastDistance = 0;
+
+    public DistanceBehaviour(DistanceLogic distance) {
+
+        this.distance = distance;
+    }
+
+    @Override
+    public void initialise() {
+
+    }
+
+    @Override
+    public void process() {
+        float measuredDistance = this.distance.getDistance();
+
+        if(measuredDistance != this.lastDistance) {
+            this.lastDistance = measuredDistance;
+
+            logger.info(Math.round(measuredDistance) + " cm");
+        }
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
---
+++ src/Hardware/Ultrasonic.java
@@ -0,0 +1,31 @@
+package Hardware;
+
+import TI.BoeBot;
+import TI.PinMode;
+
+public class Ultrasonic {
+
+    private int triggerPin;
+    private int echoPin;
+
+    public Ultrasonic(int triggerPin, int echoPin) {
+        this.triggerPin = triggerPin;
+        this.echoPin = echoPin;
+    }
+
+    public void set(boolean value) {
+        BoeBot.setMode(this.triggerPin, PinMode.Output);
+        BoeBot.digitalWrite(this.triggerPin, value);
+    }
+//
+    public boolean read() {
+        BoeBot.setMode(this.echoPin, PinMode.Input);
+        return BoeBot.digitalRead(this.echoPin);
+    }
+
+
+//    public int read() {
+//        BoeBot.setMode(this.echoPin, PinMode.Input);
+//        return BoeBot.pulseIn(this.echoPin, true, 10000);
+//    }
+}
