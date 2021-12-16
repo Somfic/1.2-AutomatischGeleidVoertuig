274191cf0d9c4c937451219bf5a8438d59761bb0
Levi de Jong <levidejong105@gmail.com>
2021-12-16 11:38:34 +0100

    Auto stash before merge of "dev" and "origin/dev"
    Line follower logic added

--- src/Logic/LineFollowerLogic.java
+++ src/Logic/LineFollowerLogic.java
@@ -7,6 +7,9 @@ public class LineFollowerLogic implements Logic {
     private final LineFollower LINE_FOLLOWER_LEFT;
     private final LineFollower LINE_FOLLOWER_MIDDLE;
     private final LineFollower LINE_FOLLOWER_RIGHT;
+    private boolean stateLeft = false;
+    private boolean stateCenter = true;
+    private boolean stateRight = false;
 
     public LineFollowerLogic(int pinLeft, int pinCenter, int pinRight) {
         this.LINE_FOLLOWER_LEFT = new LineFollower(pinLeft);
@@ -14,9 +17,38 @@ public class LineFollowerLogic implements Logic {
         this.LINE_FOLLOWER_RIGHT = new LineFollower(pinRight);
     }
 
+    public boolean getStateLeft(){
+        if (this.lineFollowerLeft.getValue() < 800){
+            this.stateLeft = true;
+        } else {
+            this.stateLeft = false;
+        }
+        return this.stateLeft;
+    }
+
+    public boolean getStateCenter(){
+        if (this.lineFollowerCenter.getValue() < 800){
+            this.stateCenter = false;
+        } else {
+            this.stateCenter = true;
+        }
+        return this.stateCenter;
+    }
+
+    public boolean getStateRight(){
+        if (this.lineFollowerRight.getValue() < 800){
+            this.stateRight = true;
+        } else {
+            this.stateRight = false;
+        }
+        return this.stateRight;
+    }
+
     @Override
     public void process() {
-
+        getStateCenter();
+        getStateLeft();
+        getStateRight();
     }
 
     @Override
