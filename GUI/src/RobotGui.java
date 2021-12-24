711f98f5bb1e97456b26eccbedd81c0debf08f51
Lucas <29230467+Somfic@users.noreply.github.com>
2021-12-24 22:10:45 +0100

    Improved GUI

--- GUI/src/RobotGui.java
+++ GUI/src/RobotGui.java
@@ -3,13 +3,12 @@ import Logic.BluetoothListener;
 import Logic.BluetoothLogic;
 import Logic.BluetoothMessage;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -31,6 +30,8 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
     private BluetoothLogic BLUETOOTH = new BluetoothLogic(this);
 
     private BorderPane mainPane;
+
+    private Pane adminPane;
     private Pane lineFollowerPane;
     private Pane manualPane;
 
@@ -39,9 +40,16 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
     private Button startStopButton;
     private Scene connectingScene;
     private Scene mainScene;
-    private TextArea loggerMessage;
 
     private Logger logger = new Logger(this);
+
+    private TextArea robotLogArea;
+    private TextArea guiLogArea;
+
+    private Button forwards;
+    private Button backwards;
+    private Button left;
+    private Button right;
 
     @Override
     public void start(Stage stage) {
@@ -50,6 +58,11 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
         Logger.setListener(this);
 
         this.stage = stage;
+
+        // Build the panes
+        lineFollowerPane = buildLineFollowerPane();
+        manualPane = buildManualPane();
+        adminPane = buildAdminPane();
 
         // Build the scenes
         mainScene = buildMainScene();
@@ -64,18 +77,56 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
         stage.show();
     }
 
+    private Pane buildAdminPane() {
+
+        HBox logs = new HBox();
+
+        VBox guiLogs = new VBox();
+        Label guiLabel = new Label("Gui");
+        guiLogArea = new TextArea();
+        guiLogArea.setEditable(false);
+        guiLogs.getChildren().addAll(guiLabel, guiLogArea);
+
+        VBox robotLogs = new VBox();
+        Label robotLabel = new Label("Robot");
+        robotLogArea = new TextArea();
+        robotLogArea.setEditable(false);
+        robotLogs.getChildren().addAll(robotLabel, robotLogArea);
+
+        logs.getChildren().addAll(guiLogs, robotLogs);
+
+        BorderPane pane = new BorderPane();
+
+        pane.setCenter(logs);
+        pane.setPadding(new Insets(10, 10, 10, 10));
+
+        return pane;
+    }
+
     public Scene buildMainScene() {
         Pane taskBar = buildTaskbar();
-
-        lineFollowerPane = buildLineFollowerPane();
-        manualPane = buildManualPane();
 
         // Build the main scene
         mainPane = new BorderPane();
         mainPane.setTop(taskBar);
 
         // Set the main component
-        setMainPane();
+        TabPane tabs = new TabPane();
+
+        Tab manualTab = new Tab("Handmatige controle", manualPane);
+        manualTab.setClosable(false);
+
+        Tab lineFollowerTab = new Tab("Lijnvolger", lineFollowerPane);
+        lineFollowerTab.setClosable(false);
+
+        Tab adminTab = new Tab("Admin", adminPane);
+        adminTab.setClosable(false);
+
+        tabs.getTabs().add(manualTab);
+        tabs.getTabs().add(lineFollowerTab);
+        tabs.getTabs().add(adminTab);
+
+        mainPane.setCenter(tabs);
 
         // Build the main scene
         return new Scene(mainPane);
@@ -98,48 +149,18 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
     }
 
     public Pane buildLineFollowerPane() {
-        // naar voren
-        // naar achteren
-        // naar links
-        // naar rechts
-        Button forwards = new Button("\uD83E\uDC81");
-        forwards.setOnAction(e -> {
-            BLUETOOTH.send("move", "forwards");
-        });
-
-        Button left = new Button("\uD83E\uDC80");
-        left.setOnAction(e -> {
-            BLUETOOTH.send("move", "left");
-        });
-
-        Button right = new Button("\uD83E\uDC82");
-        right.setOnAction(e -> {
-            BLUETOOTH.send("move", "right");
-        });
-
-        Button backwards = new Button("\uD83E\uDC83");
-        backwards.setOnAction(e -> {
-            BLUETOOTH.send("move", "backwards");
-        });
-
-        HBox hbox = new HBox(left, new VBox(forwards, backwards), right);
-        hbox.setAlignment(Pos.BOTTOM_CENTER);
-
-        // logs
-        VBox logger = new VBox();
-        Label loggerName = new Label("LOGGER");
-        loggerMessage = new TextArea();
-        logger.getChildren().addAll(loggerName, loggerMessage);
-
         BorderPane pane = new BorderPane();
-        pane.setBottom(hbox);
-        pane.setTop(logger);
+        pane.setCenter(new Label("Empty ..."));
+
+        pane.setPadding(new Insets(10, 10, 10, 10));
 
         return pane;
     }
 
     public Pane buildManualPane() {
         BorderPane pane = new BorderPane();
+
+        VBox acceleration = new VBox();
 
         // acceleratie omhoog
         Button accelerationPlus = new Button("acceleration +");
@@ -153,9 +174,38 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
             BLUETOOTH.send("acceleration", "decrease");
         });
 
-        VBox acceleration = new VBox();
         acceleration.getChildren().addAll(accelerationPlus, accelerationMin);
         pane.setBottom(acceleration);
+
+        HBox controls = new HBox();
+        forwards = new Button("\uD83E\uDC81");
+        left = new Button("\uD83E\uDC80");
+        right = new Button("\uD83E\uDC82");
+        backwards = new Button("\uD83E\uDC83");
+
+        forwards.setOnAction(e -> {
+            BLUETOOTH.send("move", "forwards");
+        });
+
+        backwards.setOnAction(e -> {
+            BLUETOOTH.send("move", "backwards");
+        });
+
+        left.setOnAction(e -> {
+            BLUETOOTH.send("move", "left");
+        });
+
+        right.setOnAction(e -> {
+            BLUETOOTH.send("move", "right");
+        });
+
+        controls.setAlignment(Pos.BOTTOM_CENTER);
+        controls.getChildren().addAll(left, new VBox(forwards, backwards), right);
+
+        pane.setCenter(new Group(controls));
+
+        pane.setPadding(new Insets(10, 10, 10, 10));
+
         return pane;
     }
 
@@ -183,24 +233,49 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
                 BLUETOOTH.send("mode", "manual");
             }
 
-            setMainPane();
+            //todo: change to correct tab
         });
 
         return new HBox(startStopButton, changeMode);
     }
 
-    public void setMainPane() {
-        if(isLineFollowingMode) {
-            mainPane.setCenter(lineFollowerPane);
-        } else {
-            mainPane.setCenter(manualPane);
-        }
-    }
-
     @Override
     public void onBluetoothMessage(BluetoothMessage message) {
-        if(message.getType().equals("log")) {
-            loggerMessage.appendText(message.getValue() + "\n");
+        if(message.getType().equals("move-direction")) {
+            if(message.getValue().equals("STATIONARY")) {
+                backwards.setDisable(false);
+                forwards.setDisable(false);
+                left.setDisable(false);
+                right.setDisable(false);
+            }
+
+            if(message.getValue().equals("LEFT")) {
+                backwards.setDisable(false);
+                forwards.setDisable(false);
+                left.setDisable(true);
+                right.setDisable(false);
+            }
+
+            if(message.getValue().equals("RIGHT")) {
+                backwards.setDisable(false);
+                forwards.setDisable(false);
+                left.setDisable(false);
+                right.setDisable(true);
+            }
+
+            if(message.getValue().equals("FORWARDS")) {
+                backwards.setDisable(false);
+                forwards.setDisable(true);
+                left.setDisable(false);
+                right.setDisable(false);
+            }
+
+            if(message.getValue().equals("BACKWARDS")) {
+                backwards.setDisable(true);
+                forwards.setDisable(false);
+                left.setDisable(false);
+                right.setDisable(false);
+            }
         }
     }
 
@@ -216,6 +291,12 @@ public class RobotGui extends Application implements BluetoothListener, LoggerLi
 
     @Override
     public void onLogMessage(LogMessage logMessage) {
-        loggerMessage.appendText(logMessage.getClassName() + "\n");
+        if(logMessage.getSource().equals("Robot")) {
+            robotLogArea.appendText(logMessage.getMessage() + "\n");
+        }
+
+        if(logMessage.getSource().equals("Gui")) {
+            guiLogArea.appendText(logMessage.getMessage() + "\n");
+        }
     }
 }
